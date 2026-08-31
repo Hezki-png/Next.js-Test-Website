4238115b36f9b37f6ac77f0b24b63706f2b663a2
Hezki-png <yehezkiel.kimley@gmail.com>
2026-08-31 18:39:33 +0700

    Initial commit

---
+++ components/hello.tsx
@@ -0,0 +1,9 @@
+'use client'; /* makes sure that this is loaded on the client side rendered */
+
+const Hello = () => {
+    console.log('I am a client side component');
+    return (
+        <div>Hello, I am the goat</div>
+    )
+}
+export default Hello
